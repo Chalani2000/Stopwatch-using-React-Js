1b70a59f1f6bfe2183fee5119f07993dc72826e4
Chalani Ekanayake <146560823+Chalani2000@users.noreply.github.com>
2024-05-19 21:26:30 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Stopwatch from './Components/Stopwatch'; // Ensure correct folder and file name
+
+function App() {
+  return (
+    <Stopwatch />
+  );
+}
+
+export default App;
